4fe1ebd9b40596aa3a7c99bf53c072c1c9c99662
raghavarahul <raghavak235@gmail.com>
2022-06-28 12:17:55 -0400

    Added Quick Sort

---
+++ Sortings/QuickSort.py
@@ -0,0 +1,25 @@
+def partition(arr, p, r):
+    pivot = arr[p]
+    i = p
+    for j in range(p+1, r+1):
+        if arr[j] < pivot:
+            i +=1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[p], arr[i] = arr[i], arr[p]
+
+    return i
+
+
+def quickssort(arr, p, r):
+
+    if p < r:
+        q = partition(arr, p, r)
+        quickssort(arr, p, q-1)
+        quickssort(arr, q+1, r)
+
+
+arr = [54, 26, 93, 17, 77, 31, 44, 55]
+
+quickssort(arr, 0, len(arr)-1)
+print(arr)
